eb6055ac60e23fa5e351ba3ec70498da21a6823e
itai chiriseri <itaineilchiriseri@gmail.com>
2023-09-27 17:25:37 +0200

    added the home view for the table to show data

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,11 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Job;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('dashboard.index');
+        $jobs = Job::all();
+        return view('dashboard.index')->with("jobs",$jobs);
     }
 }
